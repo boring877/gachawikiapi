5948dcf2103787cc175542a3f79d591e34711c79
boring877 <elfhuyace@gmail.com>
2026-02-11 14:14:48 +0000

    fixed rourting issues

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ async fn fetch(req: Request, env: Env, _ctx: worker::Context) -> Result<Response
     // url router
     let router = Router::new()
         .get_async("/", root)
-        .get_async("/api/characters", get_characters);
+        .get_async("/characters", get_characters);
 
     router.run(req, env).await
 }
